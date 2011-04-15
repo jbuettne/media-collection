49208d6a9cccbe8e9031a56660abef465cac5144
pdermitzelxk@gmail.com <pdermitzelxk@gmail.com@6a07f2ce-deba-730c-16cc-5573c717f264>
2011-04-15 12:33:47 +0000

    Quick'n'Dirty-Lösung des "nicht beenden"-Problems.

--- trunk/media collector/src/com/mediacollector/Loading.java
+++ trunk/media collector/src/com/mediacollector/Loading.java
@@ -9,16 +9,17 @@ import android.widget.TextView;
 
 public class Loading extends Activity {
 	
-	boolean resume = false;
-	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loading);        
-        TextView legalNotes = (TextView) findViewById(R.id.legalNotes);
-        legalNotes.setText("Media Collector (c) 2011\nfor more Informations "
-        		+ "visit\ncode.google.com/p/media-collection");        
-        new Thread(loadingThread).start();        
+        if (Start.loadFirst) {
+        	setContentView(R.layout.loading);        
+        	TextView legalNotes = (TextView) findViewById(R.id.legalNotes);
+        	legalNotes.setText("Media Collector (c) 2011\nfor more " 
+        			+ "Informations visit\ncode.google.com/p/media-collection");
+        	Start.loadFirst = false;
+        	new Thread(loadingThread).start();  
+        }
     }
     
     @Override
@@ -28,20 +29,10 @@ public class Loading extends Activity {
         window.setFormat(PixelFormat.RGBA_8888);
     }
     
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	if (this.resume) {
-    		startActivity(new Intent(Loading.this, com.mediacollector
-    				.Start.class));
-    	} 
-    }
-    
     private Runnable loadingThread = new Runnable() {
     	public void run() {
     		try {
 				Thread.sleep(3000);
-				resume = true;
 				startActivity(new Intent(Loading.this, com.mediacollector
 						.Start.class));
 			} catch (InterruptedException e) {
