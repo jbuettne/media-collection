615018a5876289d1effd35cc1677ab8e6b86bcc0
pdermitzelxk@gmail.com <pdermitzelxk@gmail.com@6a07f2ce-deba-730c-16cc-5573c717f264>
2011-05-11 19:54:57 +0000

    Weitere Layout-Verbesserungen

--- trunk/media collector/src/com/mediacollector/collection/audio/Artist.java
+++ trunk/media collector/src/com/mediacollector/collection/audio/Artist.java
@@ -15,22 +15,83 @@ import java.util.HashMap;
  */
 public class Artist {
 	
+	/**
+	 * Enthält alle benötigten Daten für die Objekte.
+	 * Diese können über die entsprechenden getter und setter gelesen/gesetzt 
+	 * werden.
+	 */
 	private HashMap<String, Object> data = new HashMap<String, Object>();
 	
-	public void setData(final HashMap<String, Object> dataMap) {}
-	public void setValue(final String key, final Object value) {}
+	/**
+	 * Über diese Methode kann eine Menge von Werten gesetzt werden.
+	 * Erlaubte (key-)Werte: id, name, imgPath, mbId
+	 * @param dataMap HashMap<String, Object> Map mit Key-Value-Paaren.
+	 */
+	public void setData(final HashMap<String, Object> dataMap) {		
+		if (dataMap.containsKey("id")) 
+			this.data.put("id", dataMap.get("id"));
+		if (dataMap.containsKey("name")) 
+			this.data.put("name", dataMap.get("name"));
+		if (dataMap.containsKey("imgPath")) 
+			this.data.put("imgPath", dataMap.get("imgPath"));
+		if (dataMap.containsKey("mbId")) 
+			this.data.put("mbId", dataMap.get("mbId"));		
+	}
+	
+	/**
+	 * Über diese Methode kann ein einzelner Wert gesetzt werden.
+	 * Erlaubte (key-)Werte: id, name, imgPath, mbId
+	 * @param key String Der Key.
+	 * @param value Der zu setzende Wert.
+	 */
+	public void setValue(final String key, final Object value) {
+		if (key.equals("id") || key.equals("name") || key.equals("imgPath")
+				|| key.equals("mbId")) {
+			this.data.put(key, value);
+		}
+	}
+	
+	/**
+	 * Getter.
+	 * Liefer die gesamten Daten des Objektes.
+	 * @return HashMap<String, Object>
+	 */
 	public HashMap<String, Object> getData() { 
 		return this.data;
 	}
+	
+	/**
+	 * Getter.
+	 * Liefert die ID des Artists in der Datenbank.
+	 * @return Integer
+	 */
 	public int getId() {
 		return (Integer) this.data.get("id");
 	}
+	
+	/**
+	 * Getter.
+	 * Liefert den Namen des Artists.
+	 * @return String
+	 */
 	public String getName() {
 		return (String) this.data.get("name");
 	}
+	
+	/**
+	 * Getter.
+	 * Liefert den Pfad zur Bilddatei zum Artist.
+	 * @return String
+	 */
 	public String getImgPath() {
 		return (String) this.data.get("imgPath");
 	}
+	
+	/**
+	 * Getter.
+	 * Liefert die MusicBrainz-ID des Artists.
+	 * @return String
+	 */
 	public String getMbId() {
 		return (String) this.data.get("mbId");
 	}
@@ -39,6 +100,6 @@ public class Artist {
 	public void deleteFromDb() {}
 	public void getAlbums() {}
 	public void getTracks() {}
-	public void getAll() {}
+	public static void getAll() {}
 
 }
