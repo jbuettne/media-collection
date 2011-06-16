6522966a58811c464abe1b8df3ffc5ad4afec2b1
buettner.mail <buettner.mail@gmail.com>
2011-06-16 15:24:10 +0000

    -.-

--- media collector/src/com/mediacollector/collection/Database.java
+++ media collector/src/com/mediacollector/collection/Database.java
@@ -3,7 +3,11 @@ package com.mediacollector.collection;
 import java.io.IOException;
 
 import com.mediacollector.Start;
+import com.mediacollector.collection.audio.AlbumData;
 import com.mediacollector.collection.audio.ArtistData;
+import com.mediacollector.collection.audio.TrackData;
+import com.mediacollector.collection.books.AuthorData;
+import com.mediacollector.collection.books.BookData;
 
 import android.app.Activity;
 import android.content.Context;
@@ -24,22 +28,49 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Database{
 
 	private ArtistData artist;
+	private AlbumData album;
+	private TrackData track;
+	private BookData book;
+	private AuthorData author;
 
-	private SQLiteStatement stmtInsert;
+
+	public ArtistData getArtist() {
+		return artist;
+	}
+
+	public AlbumData getAlbum() {
+		return album;
+	}
+
+	public TrackData getTrack() {
+		return track;
+	}
+
+	public BookData getBook() {
+		return book;
+	}
+
+	public AuthorData getAuthor() {
+		return author;
+	}
 
 	public Database(Context context) {
 		artist = new ArtistData(context);
+		album = new AlbumData(context);
+		track = new TrackData(context);
+//		book = new BookData(context);
+//		author = new AuthorData(context);
+		
 	}
-
-		// vernetzung mit anderen speichern...
-		// ortSpeicher = new OrtSpeicher(context);
-
-
 	/**
 	 * schlie�e die Datenbankverbindung
 	 */
 	public void closeConnection() {
-		//dbHelper.close();
+		artist.close();
+		album.close();
+		track.close();
+//		book.close();
+//		author.close();
 	}
 
 }
--- media collector/src/com/mediacollector/collection/audio/listings/ArtistListing.java
+++ media collector/src/com/mediacollector/collection/audio/listings/ArtistListing.java
@@ -1,15 +1,10 @@
 package com.mediacollector.collection.audio.listings;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import android.database.Cursor;
 import android.util.Log;
 
 import com.mediacollector.EntryListing;
-import com.mediacollector.R;
-import com.mediacollector.collection.Database;
-import com.mediacollector.collection.DatabaseHelper;
 import com.mediacollector.collection.TextImageEntry;
 import com.mediacollector.collection.audio.AlbumData;
 import com.mediacollector.collection.audio.Artist;
@@ -44,105 +39,17 @@ public class ArtistListing extends EntryListing {
 			new ArrayList<TextImageEntry[]>();
 		for (Artist artist : groupsA) {
 			ArrayList<TextImageEntry> tempGroup = albums.getAlbumsTI(artist);
-			Log.i("ArtistListing", String.valueOf(artist.mbId));
-			boolean addedGroup = groupsTI.add(tempGroup.toArray(
-					new TextImageEntry[0]));
+//			boolean addedGroup = 
+				groupsTI.add(tempGroup.toArray(new TextImageEntry[0]));
 		}
 			
-		//ArrayList<Object> children = new ArrayList<Object>();
-
-//		HashMap<TextImageEntry, TextImageEntry> childrenTest = new HashMap<TextImageEntry, TextImageEntry>();
-//		try {
-//			dbCursor = dbHelper.getReadableDatabase().rawQuery(
-//					"SELECT name FROM Artist", null);
-//			if (dbCursor == null || dbCursor.moveToFirst() == false) {}
-//	    	groups.add(dbCursor.getString(0));
-//		    while (dbCursor.moveToNext() == true) {
-//		    	groups.add(dbCursor.getString(0));
-//			}
-//		    startManagingCursor(dbCursor);
-//		    for (String artist : groups) {
-//		    	Artist tempArt = new Artist(getBaseContext(), artist);
-//		    	ArrayList<Cd> tempCd = tempArt.getAlbums();
-//	    		ArrayList<TextImageEntry> tempArtCd = new 
-//    				ArrayList<TextImageEntry>();
-//		    	for (Cd obj : tempCd) {
-//		    		TextImageEntry tempData = new 
-//		    			TextImageEntry(artist,
-//		    					getResources().getDrawable(
-//		    							R.drawable.color_red));
-//		    		tempArtCd.add(tempData);
-//		    		obj.getArtist();
-//		    		obj.getImgPath();
-//		    	}
-//		    	children.add(tempArtCd.toArray(new TextImageEntry[0]));	
-//			
-//		    }
-		    	
-			
-//		} catch (Throwable ex) {
-//			Log.e("Artist-Liste", "Artist kaputt", ex);
-//			ex.printStackTrace();
-//		} finally {
-//			dbCursor.close();
-//		}
-
-//		String[] groups = {
-//				"Aaskereia", "AC/DC", "Adele", "Aggaloch", 
-//				"Agrypnie", "Air", "Al Green", "Alice Cooper", "Alida", 
-//				"Amity in Fame", "Amon Amarth", "Amos Lee", "Amy Macdonald", 
-//				"Amy Winehouse", "Anthrax", "Apocalyptica", "Audio"
-//		};
-//		TextImageEntry[][] children = {
-//				{ new TextImageEntry("Mit Raben und Wölfen", 
-//						getResources().getDrawable(R.drawable.color_red))
-//				},
-//				{ new TextImageEntry("Stiff Upper Lip", 
-//						getResources().getDrawable(R.drawable.color_yellow))
-//				},
-//				{ new TextImageEntry("19", 
-//						getResources().getDrawable(R.drawable.color_green)), 
-//				  new TextImageEntry("21",
-//						getResources().getDrawable(R.drawable.color_yellow))
-//				},
-//				{ new TextImageEntry("Ashes Against the Grain", 
-//						getResources().getDrawable(R.drawable.color_yellow)),
-//				  new TextImageEntry("The Mantle", 
-//						getResources().getDrawable(R.drawable.color_red))
-//				},
-//				{ new TextImageEntry("F51.4", 
-//						getResources().getDrawable(R.drawable.color_green))
-//				},
-//				{ new TextImageEntry("Moon Safari",
-//						getResources().getDrawable(R.drawable.color_red))
-//				},
-//				{},{},{},{},
-//				{ new TextImageEntry("Avanger, The", 
-//						getResources().getDrawable(R.drawable.color_red)),
-//				  new TextImageEntry("Crusher, The",
-//						  getResources().getDrawable(R.drawable.color_yellow)),
-//				  new TextImageEntry("Fate of Norns", 
-//						  getResources().getDrawable(R.drawable.color_red)),
-//				  new TextImageEntry("Twilight of the Thunder God",  
-//						  getResources().getDrawable(R.drawable.color_yellow)),
-//				  new TextImageEntry("Once Sent from the Golden Hall", 
-//						  getResources().getDrawable(R.drawable.color_yellow)),
-//			      new TextImageEntry("Sutur Rising",
-//						  getResources().getDrawable(R.drawable.color_red)),
-//				  new TextImageEntry("Versus the World", 
-//						  getResources().getDrawable(R.drawable.color_red)),
-//			      new TextImageEntry("With Oden on Our Side",
-//						  getResources().getDrawable(R.drawable.color_red))
-//				},
-//				{},{},{},{},{},{}
-//		};
-	    
 		this.groups = groups.toArray(new String[0]);
 		this.children = groupsTI.toArray(new TextImageEntry[0][0]); //children.toArray(new TextImageEntry[0][0]);
 	}
 	@Override
 	protected void onDestroy() {
 		artists.close();
+		albums.close();
 		super.onDestroy();
 	}
 }
