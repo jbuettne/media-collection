e4158e9369d7ab3e0dd74f2f4933fd9608df4a27
pdermitzelxk <pdermitzelxk@gmail.com>
2011-06-16 14:00:32 +0000

    Für David

--- media collector/src/com/mediacollector/tools/Preferences.java
+++ media collector/src/com/mediacollector/tools/Preferences.java
@@ -2,8 +2,6 @@ package com.mediacollector.tools;
 
 import com.mediacollector.R;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
@@ -14,18 +12,11 @@ import android.preference.PreferenceActivity;
  */
 public class Preferences extends PreferenceActivity {
 	
-	public static final String PREF_FILE = "MCPrefs";  
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.preferences);
 	    getListView().setBackgroundColor(Color.BLACK);
-	}
-	
-	public static final SharedPreferences getPreferences(final Context con) {
-		return con.getSharedPreferences(con.getPackageName() + "_preferences",
-				MODE_PRIVATE);
 	}
 
 }
