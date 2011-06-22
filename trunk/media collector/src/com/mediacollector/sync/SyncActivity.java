4be08ff7471de60f943f4d8d13503c44d1961c2c
pdermitzelxk@gmail.com <pdermitzelxk@gmail.com@6a07f2ce-deba-730c-16cc-5573c717f264>
2011-06-22 10:05:38 +0000

    Synchronisation als eigener Thread im Hintergrund. Layout wird noch überarbeitet. Implementierung mglw. etwas "Von Hinten durch die Brust ins Auge" mit Observer-Pattern und eigenen (Java-)Threads.

---
+++ trunk/media collector/src/com/mediacollector/sync/SyncActivity.java
@@ -0,0 +1,36 @@
+package com.mediacollector.sync;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.mediacollector.R;
+import com.mediacollector.tools.Observer;
+
+/**
+ * Zeigt eine ProgressBar an und startet die Synchronisation (im Augenblick 
+ * mittels Dropbox) im Hintergrund (in eigenem Thread).
+ * @author Philipp Dermitzel
+ */
+public class SyncActivity extends Activity implements Observer {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.sync);  
+        
+        Dropbox dropbox = new Dropbox(this);
+        dropbox.addObserver(this);
+        new Thread(dropbox).start();
+	}
+
+	/**
+	 * Schließt die Activity wenn der Thread durch das Observable darüber 
+	 * benachrichtigt wird, dass die Hintergrundaktion beendet ist.
+	 */
+	public void updateObserver() {
+		this.finish();
+	}
+
+}
