08dca6e4803a208d474b85e99bac31d4f0c125bd
pdermitzelxk@gmail.com <pdermitzelxk@gmail.com@6a07f2ce-deba-730c-16cc-5573c717f264>
2011-06-22 10:08:13 +0000

    Syncen auch über das Menu möglich.

--- trunk/media collector/src/com/mediacollector/tools/RegisteredActivity.java
+++ trunk/media collector/src/com/mediacollector/tools/RegisteredActivity.java
@@ -3,6 +3,7 @@ package com.mediacollector.tools;
 import com.mediacollector.InfoPopUp;
 import com.mediacollector.R;
 import com.mediacollector.ScanBarcode;
+import com.mediacollector.sync.SyncActivity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -33,6 +34,9 @@ public class RegisteredActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
+    		case R.id.menu_sync:
+    			startActivity(new Intent(getBaseContext(), SyncActivity.class));
+    			return true;
     		case R.id.menu_scan:
     			startActivity(new Intent(getBaseContext(), ScanBarcode.class));
     			return true;
